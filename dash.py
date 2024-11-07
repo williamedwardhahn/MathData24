937d5e7e49f7c6110c46b6923bc2d1ebdeba67e9
William E Hahn <williamedwardhahn@gmail.com>
2024-11-07 16:15:54 -0500

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template_string, request, redirect, url_for, send_file
 import matplotlib.pyplot as plt
 import io
-from pyngrok import ngrok
-
 app = Flask(__name__)
 
 # Status dictionary to store the state of each light
